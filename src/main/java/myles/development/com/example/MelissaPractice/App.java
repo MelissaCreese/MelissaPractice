e0bd22114a5108bbeb47cc9cef84f780713fad02
Myles Creese <macreese.mc@gmail.com>
2016-08-13 09:48:11 -0500

    Initial Commit

---
+++ src/main/java/myles/development/com/example/MelissaPractice/App.java
@@ -0,0 +1,13 @@
+package myles.development.com.example.MelissaPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
