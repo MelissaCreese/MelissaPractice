1f1cb33eff495570c8d2b5ad56b1ae36ca6f0152
Myles Creese <macreese.mc@gmail.com>
2016-08-13 10:08:18 -0500

    Factorial Practice Addition

---
+++ src/main/java/myles/development/com/example/MelissaPractice/FactorialPractice.java
@@ -0,0 +1,29 @@
+package myles.development.com.example.MelissaPractice;
+
+import java.util.*; 
+import java.io.*;
+
+public class FactorialPractice {  
+  public static int FirstFactorial(int num) { 
+  
+    // code goes here   
+    /* Note: In Java the return type of a function and the 
+       parameter types being passed are defined, so this return 
+       call must match the return type of the function.
+       You are free to modify the return type. */
+       
+    return num;
+    
+  } 
+  
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(FirstFactorial(s.nextInt())); 
+  }   
+  
+}
+
+
+
+
