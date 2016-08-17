399289d39ed9f50b895b480add69444d96263611
MelissaC <melissa_creese@yahoo.com>
2016-08-16 22:56:21 -0500

    loser

---
+++ src/main/java/myles/development/com/example/MelissaPractice/Orange.java
@@ -0,0 +1,21 @@
+package myles.development.com.example.MelissaPractice;
+import java.util.Scanner;
+
+public class Orange {
+	public void main(String args[]){
+		@SuppressWarnings("unused")
+		Scanner bucky = new Scanner(System.in);
+		int tuna = 5;
+		@SuppressWarnings("unused")
+		int bass = 18;
+		tuna *= 8;
+		
+		
+		System.out.println(tuna);
+		
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/myles/development/com/example/MelissaPractice/fruit.java
@@ -0,0 +1,16 @@
+package myles.development.com.example.MelissaPractice;
+import java.util.Scanner;
+
+public class fruit {
+	public static void main(String args[]){
+		@SuppressWarnings("unused")
+		Scanner bucky = new Scanner(System.in);
+		int girls, boys, people;
+		girls = 11;
+		boys = 3;
+		people = girls / boys;
+		System.out.println(people);
+		
+	}
+
+}
